e04e57cf6b8f94534a475513ce862661026f3ca9
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-07-13 19:10:11 +0100

    player can move

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const player = new Player(225, 750)
 function Player(x, y) {
   this.x = x
   this.y = y
+  this.direction = 0
+  this.speed = 10
   this.sprite = document.createElement('div')
   
   this.insertPlayer = function () {
@@ -12,7 +14,30 @@ function Player(x, y) {
     this.sprite.style.top = this.y + 'px'
     board.appendChild(this.sprite)
   }
+
+  this.move = function () {
+    this.x += this.speed * this.direction
+    this.sprite.style.left = this.x + 'px'
+  }
 }
 
 player.insertPlayer()
 
+window.addEventListener('keydown', function(e) {
+  switch (e.key) {
+    case 'ArrowLeft':
+      player.direction = -1
+      break
+    case 'ArrowRight':
+      player.direction = 1
+      break
+  }
+  player.move()
+
+})
+
+window.addEventListener('keyup', function(e) {
+  if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+    player.direction = 0
+  }
+})
