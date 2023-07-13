1031b452232943f78d4e3415dcfea39bcb1ee834
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-07-13 19:32:33 +0100

    player collides with borders

--- player.js
+++ player.js
@@ -3,7 +3,7 @@ function Player(x, y, parent) {
   this.x = x
   this.y = y
   this.direction = 0
-  this.speed = 10
+  this.speed = 5
   this.sprite = document.createElement('div')
 
   this.insertPlayer = function () {
@@ -14,8 +14,11 @@ function Player(x, y, parent) {
   }
 
   this.move = function () {
-    self.x += self.speed * self.direction
-    self.sprite.style.left = self.x + 'px'
+    let newX = self.x + self.speed * self.direction
+    if (newX >= 0 && newX <= 450) {
+      self.x = newX
+      self.sprite.style.left = self.x + 'px'
+    }
   }
 }
 
