0a874750493b6791e40b74f00b6a8a7b4dd96f54
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-07-13 20:19:48 +0100

    enemies can move

--- enemy.js
+++ enemy.js
@@ -1,6 +1,8 @@
 function Enemy(x,y, parent) {
+  var self = this
   this.x = x
   this.y = y
+  this.speed = 10
   this.sprite = document.createElement('div')
 
   this.insertEnemy = function () {
@@ -9,6 +11,13 @@ function Enemy(x,y, parent) {
     this.sprite.style.top = this.y + 'px'
     parent.appendChild(this.sprite)
   }
+
+  this.move = function () {
+    self.y += self.speed
+    self.sprite.style.top = self.y + 'px'
+  }
+
+  this.timerId = setInterval(this.move, 100)
 }
 
 export { Enemy }
